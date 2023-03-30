9c19520f84592c9fdbb39b153d7c2a5c2aeb6fe1
Anilrocky <anilkumarac96@gmail.com>
2023-03-30 12:12:28 +0530

    [Anil] Add:Given message, return SAD or HAPPY

--- MoodAnalyser/MoodAnalyzer.cs
+++ MoodAnalyser/MoodAnalyzer.cs
@@ -2,6 +2,16 @@
 {
     public class MoodAnalyzer
     {
-
+        string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        }
     }
 }
--- MoodAnalyserTest/MoodAnalyzerTest.cs
+++ MoodAnalyserTest/MoodAnalyzerTest.cs
@@ -1,11 +1,24 @@
+using MoodAnalyser;
+
 namespace MoodAnalyserTest
 {
     public class Tests
     {     
         [Test]
-        public void Test1()
+        public void GivenSadMood_ShouldReturn_Sad()
         {
-            Assert.Pass();
+            string message = "Iam in Sad Mood";
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
+            Assert.AreEqual("SAD",result);
+        }
+        [Test]
+        public void GivenAnyMood_ShouldReturn_Happy()
+        {
+            string message = "Iam in Any Mood";
+            MoodAnalyzer mood = new MoodAnalyzer(message);
+            string result = mood.AnalyzeMood();
+            Assert.AreEqual("HAPPY", result);
         }
     }
 }
