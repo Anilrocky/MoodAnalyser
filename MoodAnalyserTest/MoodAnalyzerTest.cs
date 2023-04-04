a612992b05de8385b323301c20709dc4a482f3ab
Anilrocky <anilkumarac96@gmail.com>
2023-04-04 07:25:07 +0530

    [Anil] Add:Given Class name when improper should throw exception

--- MoodAnalyserTest/MoodAnalyzerTest.cs
+++ MoodAnalyserTest/MoodAnalyzerTest.cs
@@ -48,5 +48,43 @@ namespace MoodAnalyserTest
                 Assert.AreEqual(ex.Message, "Message is Empty");
             }
         }
+        [Test]
+        public void GivenClassName_ShouldReturnMoodAnalyzeObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyzer(message);
+            object obj = MoodAnalyzerFactory.CreateMoodAnalyze("MAnalyzer.MoodAnalyzer", "MoodAnalyzer");
+            expected.Equals(obj);
+        }
+        [Test]
+        public void GivenClassNameImproper_ShouldThrowException()
+        {
+            try
+            {
+                string message = null;
+                object expected = new MoodAnalyzer(message);
+                object obj = MoodAnalyzerFactory.CreateMoodAnalyze("MAnalyzer.Mood_analyzer", "MoodAnalyzer");
+                expected.Equals(obj);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Class Not Found");
+            }
+        }
+        [Test]
+        public void GivenConstructorNameImproper_ShouldThrowException()
+        {
+            try
+            {
+                string message = null;
+                object expected = new MoodAnalyzer(message);
+                object obj = MoodAnalyzerFactory.CreateMoodAnalyze("MAnalyzer.MoodAnalyzer", "Mood_analyzer");
+                expected.Equals(obj);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Constructor Not Found");
+            }
+        }
     }
 }
